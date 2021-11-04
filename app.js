dccd1740d17e8b731ed368dbd617fd401d0b7d50
Jean C. Herrera <jeancarlosherreram@gmail.com>
2021-11-04 15:53:12 -0400

    local storage for theme added

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+
     const themeStyleSheet = document.getElementById("theme");
     const toggleButton = document.getElementById("toggle-btn");
     const togglePhoto = document.getElementById("photo");
     const toggleIcon = document.getElementById("toggle-icon");
+    const storedTheme = localStorage.getItem("preferredTheme");
+    const storedPhoto = localStorage.getItem("preferredPhoto");
+    const storedIcon = localStorage.getItem("preferredIcon");
+
+    if (storedTheme && storedPhoto && storedIcon) {
+        themeStyleSheet.href = storedTheme;
+        togglePhoto.src = storedPhoto;
+        toggleIcon.innerText = storedIcon;
+    }
+
     toggleButton.addEventListener("click", () => {
         if (themeStyleSheet.href.includes("dark")) {
             themeStyleSheet.href = "light-theme.css";
@@ -15,5 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
             togglePhoto.src = "img/black_and_white.jpg";
             toggleIcon.innerText = "wb_sunny"
         }
+
+        localStorage.setItem("preferredTheme", themeStyleSheet.href)
+        localStorage.setItem("preferredPhoto", togglePhoto.src)
+        localStorage.setItem("preferredIcon", toggleIcon.innerText)
     })
 })
