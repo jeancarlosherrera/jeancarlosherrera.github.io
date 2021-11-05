b6accd2cbf9e5de37582b3e834383895b958908d
Jean C. Herrera <jeancarlosherreram@gmail.com>
2021-11-05 16:07:13 -0400

    updated about section

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    // Initial version to add functionality, must refactor final version
+
     const themeStyleSheet = document.getElementById("theme");
     const toggleButton = document.getElementById("toggle-btn");
     const togglePhoto = document.getElementById("photo");
     const toggleIcon = document.getElementById("toggle-icon");
+    const about = document.getElementById("about");
+
     const storedTheme = localStorage.getItem("preferredTheme");
     const storedPhoto = localStorage.getItem("preferredPhoto");
     const storedIcon = localStorage.getItem("preferredIcon");
+    const storedAbout = localStorage.getItem("about");
 
-    if (storedTheme && storedPhoto && storedIcon) {
+
+    if (storedTheme && storedPhoto && storedIcon && storedAbout) {
         themeStyleSheet.href = storedTheme;
         togglePhoto.src = storedPhoto;
         toggleIcon.innerText = storedIcon;
+        about.href = storedAbout;
     }
 
     toggleButton.addEventListener("click", () => {
         if (themeStyleSheet.href.includes("dark")) {
             themeStyleSheet.href = "light-theme.css";
             togglePhoto.src = "img/color.jpg";
-            toggleIcon.innerText = "mode_night"
+            toggleIcon.innerText = "mode_night";
+            about.href = "about-light.html";
 
         } else {
             themeStyleSheet.href = "dark-theme.css";
             togglePhoto.src = "img/black_and_white.jpg";
-            toggleIcon.innerText = "wb_sunny"
+            toggleIcon.innerText = "wb_sunny";
+            about.href = "about-dark.html";
         }
 
-        localStorage.setItem("preferredTheme", themeStyleSheet.href)
-        localStorage.setItem("preferredPhoto", togglePhoto.src)
-        localStorage.setItem("preferredIcon", toggleIcon.innerText)
+        localStorage.setItem("preferredTheme", themeStyleSheet.href);
+        localStorage.setItem("preferredPhoto", togglePhoto.src);
+        localStorage.setItem("preferredIcon", toggleIcon.innerText);
+        localStorage.setItem("about", about.href);
     })
 })
